e01783267e0180f2e7eaeca039a3ca4367dd7112
Ethan Favia <efavia215@live.com>
2017-05-31 11:33:59 -0400

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+// decortator to show typescript when its compiling that this is a component
+@Component({
+    selector: 'app-server',
+    templateUrl: ''
+})
+
+export class ServerComponent {
+
+}
