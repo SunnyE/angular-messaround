99d2993c36415dc0ee429d2f64de848427d35738
Ethan Favia <efavia215@live.com>
2017-05-31 12:44:03 -0400

    setting up server route

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 // decortator to show typescript when its compiling that this is a component
 @Component({
     selector: 'app-server',
-    templateUrl: ''
+    templateUrl: './server.component.html'
 })
 
 export class ServerComponent {
